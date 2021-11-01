e27067c3905e2439512799722444343b9a9b783d
Mirko <18010385@student.hhs.nl>
2021-11-01 10:14:06 +0100

    Eerste opzet

---
+++ Dashboard.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+import plotly.figure_factory as ff
+import plotly.express as px
+import io
+import time
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import os
+from streamlit_folium import folium_static
+import folium
+
+
+# Enkele standaard opties
+st.set_page_config(layout="wide") # Zorgt voor default wide op streamlit
+pd.set_option('display.max_columns', None) # Print alles van de DataFrame pandas
+
+
+# Maak een titel
+st.title('Dashboard')
