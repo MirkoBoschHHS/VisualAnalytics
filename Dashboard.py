c9ba6b5b7e8ce84dbd4fa5b97f011cf75a814cf4
Mirko <18010385@student.hhs.nl>
2021-11-01 10:18:43 +0100

    Thema aangepast

--- Dashboard.py
+++ Dashboard.py
@@ -13,6 +13,24 @@ from streamlit_folium import folium_static
 import folium
 
 
+# Primary accent for interactive elements
+primaryColor = '#7792E3'
+
+# Background color for the main content area
+backgroundColor = '#273346'
+
+# Background color for sidebar and most interactive widgets
+secondaryBackgroundColor = '#B9F1C0'
+
+# Color used for almost all text
+textColor = '#FFFFFF'
+
+# Font family for all text in the app, except code blocks
+# Accepted values (serif | sans serif | monospace)
+# Default: "sans serif"
+font = "sans serif"
+
+
 # Enkele standaard opties
 st.set_page_config(layout="wide") # Zorgt voor default wide op streamlit
 pd.set_option('display.max_columns', None) # Print alles van de DataFrame pandas
