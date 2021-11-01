f2b4e4988bfd1dbdd9b1cc4dc89a921d1914ee1f
Mirko <18010385@student.hhs.nl>
2021-11-01 17:18:46 +0100

    Toevoeging van map

--- Dashboard.py
+++ Dashboard.py
@@ -15,6 +15,7 @@ import folium
 import cbsodata
 import datetime
 
+
 import navigation
 
 
--- navigation.py
+++ navigation.py
@@ -1,5 +1,20 @@
 import streamlit as st
 import base64
+import geopandas as gpd
+import streamlit as st
+import pandas as pd
+from streamlit_autorefresh import st_autorefresh
+import numpy as np
+import requests
+import plotly.figure_factory as ff
+import plotly.express as px
+import io
+import time
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import os
+from streamlit_folium import folium_static
+import folium
 
 
 def navigation(nav, df_crimi2):
@@ -9,9 +24,30 @@ def navigation(nav, df_crimi2):
 
 
     elif nav == "results":
-        st.title('Results List')
-        for item in range(25):
-            st.write(f'Results {item}')
+        df_crimi = df_crimi2
+        polygonen = gpd.read_file('gemeente_2020_v2.shp')
+        polygonen[polygonen['H2O'] == 'NEE']
+        polygonen = polygonen[['GM_NAAM', 'geometry']]
+        polygonen.rename(columns={'GM_NAAM': 'RegioS'}, inplace=True)
+        df_crimi_kaart = df_crimi[
+            (df_crimi['Perioden'] == '2020') & (df_crimi['SoortMisdrijf'] == 'Misdrijven, totaal')]
+
+        m = folium.Map(location=[52.25, 5.4],
+                       tiles='Carto DB Positron',
+                       zoom_start=8)
+
+        folium.Choropleth(geo_data=polygonen,
+                          name='geometry',
+                          data=df_crimi_kaart,
+                          columns=['RegioS', 'GeregistreerdeMisdrijvenPer1000Inw_3'],
+                          key_on='feature.properties.RegioS',
+                          fill_color='YlOrRd',
+                          fill_opacity=0.7,
+                          line_opacity=0.2,
+                          legend_name='Geregistreerde misdrijven per 1000 inwoners',
+                          nan_fill_color='black').add_to(m)
+
+        st.folium_static(m)
 
     elif nav == "Dataframe":
         st.title("Het gedownloade dataframe")
